def057809f09c28233170411757ef4283b3b0fb8
akeit0 <90429982+Akeit0@users.noreply.github.com>
2026-06-07 01:14:06 +0900

    sandbox

--- sandbox/MinimumPlaybackSandbox/Program.cs
+++ sandbox/MinimumPlaybackSandbox/Program.cs
@@ -4,7 +4,7 @@ using static MinimumPlayback.PlaybackTask;
 var playback1 = Playback.Create(playback => Scenario(playback, 6));
 var playback2 = Playback.Create(playback => Scenario(playback, 4));
 
-for (var i = 0; i < 128 && (!playback1.IsCompleted || !playback2.IsCompleted); i++)
+for (var i = 0; i < 512 && (!playback1.IsCompleted || !playback2.IsCompleted); i++)
 {
     var moveBackTurn = i % 3 == 2;
     if ((i & 1) == 0)
@@ -33,9 +33,7 @@ Dump(playback2);
 
 static async PlaybackTask Scenario(Playback playback, int n)
 {
-    await Checkpoint($"start");
-    var value = await Fib(n);
-    await Checkpoint($"end={value}");
+    _ = await Fib(n);
 }
 
 static async PlaybackTask<int> Fib(int n)
@@ -74,6 +72,6 @@ static void Dump(Playback playback)
 {
     foreach (var record in playback.Records)
         Console.WriteLine(
-            $"{record.Index}:\t{string.Concat(Enumerable.Repeat("| ", record.Depth))}{record.Role} {record.Label} depth={record.Depth} parent={record.ParentIndex}"
+            $"{record.Index}:\t{string.Concat(Enumerable.Repeat("| ", record.Depth))}{record.Label} depth={record.Depth} parent={record.ParentIndex}"
         );
 }
